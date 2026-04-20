2239cb7e82f1870fa844fa2472a20e9f423a2130
GabrielGarciaRodri <sky.ggarcia@gmail.com>
2026-04-20 15:11:19 -0500

    build: :building_construction: Build config improvement

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port);
 
-  console.log(`🚀 URL Shortener running on http://localhost:${port}`);
+  console.log(`URL Shortener running on http://localhost:${port}`);
 }
 
 bootstrap();
